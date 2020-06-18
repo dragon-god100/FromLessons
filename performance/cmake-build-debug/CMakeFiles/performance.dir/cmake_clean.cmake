a606297bfebeef9c7d288e5dd70d3c032d9b2db3
Sapir Shemer <lesscomplexity@gmail.com>
2020-06-18 16:53:04 +0300

    Performance leasson

---
+++ performance/cmake-build-debug/CMakeFiles/performance.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/performance.dir/main.cpp.o"
+  "libperformance.dll.a"
+  "performance.exe"
+  "performance.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/performance.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ performance/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <chrono>
+#include <random>
+
+int main() {
+    int s = 640000;
+    int *myarr = new int[s];
+
+    // 1-Step Linear memory access
+    auto start = std::chrono::steady_clock::now();
+    for(int i = 0; i < s; i++) {
+        myarr[i] = 100;
+    }
+    auto end = std::chrono::steady_clock::now();
+    std::cout << "Time elapsed: " << std::chrono::duration_cast<std::chrono::microseconds>(end - start).count() << "us" << std::endl;
+
+    // n-Step Linear memory access
+    start = std::chrono::steady_clock::now();
+    for(int i = 0; i < s * 33; i += 33) {
+        myarr[i % s] = 100;
+    }
+    end = std::chrono::steady_clock::now();
+    std::cout << "Time elapsed: " << std::chrono::duration_cast<std::chrono::microseconds>(end - start).count() << "us" << std::endl;
+
+    // Random memory access
+    start = std::chrono::steady_clock::now();
+    for(int i = 0; i < s; i++) {
+        myarr[rand() % s] = 100;
+    }
+    end = std::chrono::steady_clock::now();
+    std::cout << "Time elapsed: " << std::chrono::duration_cast<std::chrono::microseconds>(end - start).count() << "us" << std::endl;
+    return 0;
+}
